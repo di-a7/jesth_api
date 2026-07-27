1b7ddbbdfe6b702dedc6751cc74769e62324a585
diya <shresthadiya59@gmail.com>
2026-07-27 09:01:48 +0545

    postgres integrate, admin customization

--- rms/admin.py
+++ rms/admin.py
@@ -1,10 +1,26 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Menu)
-admin.site.register(Table)
-admin.site.register(Order)
-admin.site.register(OrderMenu)
+class CategoryAdmin(admin.ModelAdmin):
+   list_display = ('id','name')
 
-# admin customization
+admin.site.register(Category, CategoryAdmin)
+
+class MenuAdmin(admin.ModelAdmin):
+   list_display = ('id','name','price','category')
+   search_fields = ('name',)
+   list_filter=('category',)
+admin.site.register(Menu, MenuAdmin)
+
+admin.site.register(Table)
+
+class OrderMenuInline(admin.TabularInline):
+   model = OrderMenu
+   autocomplete_fields = ("menu",)
+
+class OrderAdmin(admin.ModelAdmin):
+   list_display = ('id', 'user','total_price','status')
+   inlines = [OrderMenuInline]
+
+admin.site.register(Order, OrderAdmin)
+# admin.site.register(OrderMenu)
